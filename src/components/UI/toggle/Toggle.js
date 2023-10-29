e41ea5b366f0a61f66a45542bd4fe08f4deb9379
Yurii <yuriiyablonskyi.i@gmail.com>
2023-10-29 15:52:32 +0100

    add adaptive header

--- src/components/UI/toggle/Toggle.js
+++ src/components/UI/toggle/Toggle.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import styles from './Toggle.module.sass'
 
-const Toggle = () => (
-  <div className={styles.toggle}>
-    <div className={styles.item}></div>
-    <div className={styles.item}></div>
-    <div className={styles.item}></div>
-  </div>
-)
+const Toggle = ({ toggleNav }) => {
+
+  const [isActive, setIsActive] = useState(false)
+  const toggleHandler = () => {
+    setIsActive(!isActive)
+    toggleNav()
+  }
+
+  return (
+    <div className={`${styles.toggle} ${isActive && styles.active}`} onClick={toggleHandler}>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+    </div>
+  )
+}
 
 export default Toggle
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,23 +2,32 @@ import styles from './Header.module.sass'
 import logo from '../../logo.svg'
 import NavItem from '../navItem/NavItem'
 import Button from '../UI/button/Button'
+import Toggle from '../UI/toggle/Toggle'
+import { useState } from 'react'
 
-const Header = ({ onVisible }) => (
-  <header className={styles.header}>
-    <a href="#">
-      <img src={logo} alt="logo" className={styles.img} />
-    </a>
+const Header = ({ onVisible }) => {
+  const [isNavOpen, setIsNavOpen] = useState(false)
+  const toggleNav = () => setIsNavOpen(!isNavOpen)
 
-    <nav>
-      <NavItem text="Page-1" />
-      <NavItem text="Page-2" />
-      <NavItem text="Page-3" />
-      <NavItem text="Page-4" />
-      <NavItem text="Page-5" />
-      <Button text="Log in" onVisible={onVisible} />
-    </nav>
-  </header>
-)
+  return (
+    <header className={styles.header}>
+      <a href="#">
+        <img src={logo} alt="logo" className={styles.img} />
+      </a>
+
+      <nav className={`${styles.nav} ${isNavOpen && styles.visible}`}>
+        <NavItem text="Page-1" />
+        <NavItem text="Page-2" />
+        <NavItem text="Page-3" />
+        <NavItem text="Page-4" />
+        <NavItem text="Page-5" />
+        <Button text="Log in" onVisible={onVisible} />
+      </nav>
+
+      <Toggle toggleNav={toggleNav} />
+    </header>
+  )
+}
 
 export default Header
 
